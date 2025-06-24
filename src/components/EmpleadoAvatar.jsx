b72b3297d6a11b8ef620ecca64a91d9b863f7740
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-06-24 18:16:17 -0300

    importe las imagenes

--- src/components/EmpleadoAvatar.jsx
+++ src/components/EmpleadoAvatar.jsx
@@ -3,7 +3,7 @@ import "./EmpleadoAvatar.css";
 const EmpleadoAvatar = ({ pic, name }) => {
   return (
     <img
-      className="border border-danger ms-1 w-sm-15"
+      className="border border-danger ms-1 w-sm-15 img-fluid"
       src={pic}
       alt={`Foto de ${name}`}
     />
--- src/components/EmpleadoList.jsx
+++ src/components/EmpleadoList.jsx
@@ -1,4 +1,13 @@
 import EmpleadoRow from "./EmpleadoRow";
+import ceo from "../assets/ceo.png";
+import cmo from "../assets/cmo.png";
+import cfo from "../assets/cfo.png";
+import cto from "../assets/cto.png";
+import artDirector from "../assets/art-director.png";
+import frontendDev from "../assets/frontend-dev.jpg";
+import digitalStrategist from "../assets/digital-strategist.jpg";
+import backendDev from "../assets/backend-dev.webp";
+import devOpsEngineer from "../assets/devops.png";
 
 const EmpleadoList = () => {
   let empleados = [
@@ -7,63 +16,63 @@ const EmpleadoList = () => {
       fullName: "Laya Dueñas",
       title: "CEO",
       department: "Business",
-      pic: "empleado01.png",
+      pic: ceo,
     },
     {
       id: 2,
       fullName: "Astryd Vallés",
       title: "CMO",
       department: "Marketing",
-      pic: "empleado02.png",
+      pic: cmo,
     },
     {
       id: 3,
       fullName: "Shantell Meza",
       title: "CFO",
       department: "Business",
-      pic: "empleado03.png",
+      pic: cfo,
     },
     {
       id: 4,
       fullName: "Sergio Ocampo",
       title: "CTO",
       department: "Engineering",
-      pic: "empleado04.png",
+      pic: cto,
     },
     {
       id: 5,
       fullName: "Ares Jiménez",
       title: "Art Director",
       department: "Marketing",
-      pic: "empleado05.png",
+      pic: artDirector,
     },
     {
       id: 6,
       fullName: "Marta Pérez",
       title: "Frontend Dev",
       department: "Engineering",
-      pic: "empleado06.png",
+      pic: frontendDev,
     },
     {
       id: 7,
       fullName: "Ellen Balderas",
       title: "Digital Strategist",
       department: "Marketing",
-      pic: "empleado07.png",
+      pic: digitalStrategist,
     },
     {
       id: 8,
       fullName: "Cynthia Valentín",
       title: "Backend Dev",
       department: "Engineering",
-      pic: "empleado08.png",
+      pic: backendDev,
     },
     {
       id: 9,
       fullName: "Bernard Jung",
       title: "DevOps Engineer",
       department: "Engineering",
-      pic: "empleado09.png",
+      pic: devOpsEngineer,
     },
   ];
   return (
